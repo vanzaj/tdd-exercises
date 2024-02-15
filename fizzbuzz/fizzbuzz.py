3d08a2821c86178f558326e84dcdce33f01e9471
Ivan <vanzaj@gmail.com>
2024-02-15 20:19:53 +0800

    sep and error

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,12 +1,14 @@
 def fizzbuzz(number: int, divisors: dict = {"Fizz": 3, "Buzz": 5, "FizzBuzz": 15}) -> str:
+    if number == 0:
+        raise ValueError()
     for name, divisor in sorted(divisors.items(), key=lambda x: x[1], reverse=True):
         if is_divisible(number, divisor):
             return name
     return str(number)
 
 
-def fizzbuzz_print(upto: int) -> str:
-    return ",".join([fizzbuzz(n) for n in range(1, upto+1)])
+def fizzbuzz_print(upto: int, sep: str = ",") -> str:
+    return sep.join([fizzbuzz(n) for n in range(1, upto+1)])
 
 
 def is_divisible(number: int, divisor: int) -> bool:
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,14 +1,16 @@
 # Basic FizzBuzz:
 # create output following the pattern:
-# "1, 2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16" up to given number.
+# "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16" up to given number.
 #
 # Extended FizzBuzz:
+# add custom separator: FizzBuzzExtended(3, sep='\n')
 # pass divisor arguments: FizzBuzzExtended(100, {"fizz": 3, "buzz": 4})
 # pass any number of divisor arguments: FizzBuzzExtended(100, {"fizz": 3, "buzz": 4, "foo": 8, "bar": 9})
 
 # unittest or pytest? pytest
 import pytest
 from fizzbuzz import fizzbuzz, fizzbuzz_print
+
 
 @pytest.mark.parametrize("number, expected", [
     (1, "1"),
@@ -31,9 +33,20 @@ def test_fizzbuzz_for_normal_numbers(number, expected):
 def test_fizzbuzz_for_fizz_and_buzz(number, expected):
     assert fizzbuzz(number) == expected
 
+
 def test_printing_fizzbuzz():
     expected = "1,2,Fizz,4,Buzz"
     assert fizzbuzz_print(5) == expected
+
+
+def test_printing_fizzbuzz_with_custom_separator():
+    expected = "1\n2\nFizz"
+    assert fizzbuzz_print(3, sep="\n") == expected
+
+
+def test_fizzbuzz_raises_error_for_zero():
+    with pytest.raises(ValueError):
+        result = fizzbuzz(0)
 
 
 def test_fizzbuzz_with_divisor_args():
