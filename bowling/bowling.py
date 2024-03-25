af37db965ba92cd3b567fc53207d5a58e858f102
Ivan <vanzaj@gmail.com>
2024-03-25 22:05:27 +0800

    restart

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,16 +1,6 @@
-class BowlingGame():
+class BowlingGame:
+    def rolls(self, rolls):
+        self._rolls = rolls
 
-  def pin_to_int(self, pin):
-    if pin == '-':
-      return 0
-    return int(pin)
-
-  def line(self, pins):
-    pins = [self.pin_to_int(p) for p in pins]
-    self._frames = list(zip(pins[::2], pins[1::2]))
-
-  def score(self):
-    score = 0
-    for (a, b) in self._frames:
-      score += a + b
-    return score
+    def score(self):
+        return sum(self._rolls)
--- bowling/test_bowling.py
+++ bowling/test_bowling.py
@@ -1,21 +1,17 @@
 import pytest
 from bowling import BowlingGame
 
+
 @pytest.fixture
 def game():
-  yield BowlingGame()
+    yield BowlingGame()
+
 
 def test_new_bowling_game(game):
-  assert isinstance(game, object)
+    assert isinstance(game, object)
 
-def test_line_to_frames(game):
-  game.line('1234')
-  assert game._frames == [(1, 2), (3, 4)]
 
-def test_score_1234(game):
-  game.line('1234')
-  assert game.score() == 10
-
-def test_miss(game):
-  game.line('8-')
-  assert game.score() == 8
+def test_rolls(game):
+    game.rolls([1, 2])
+    assert game._rolls == [1, 2]
+    assert game.score() == 3
