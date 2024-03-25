2bb0602c5a42ceda90ebcf4218be61107f52a334
Ivan <vanzaj@gmail.com>
2024-03-25 09:44:03 +0800

    refactor

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,12 +1,16 @@
 class BowlingGame():
 
-  def pin_to_score(self, pin):
+  def pin_to_int(self, pin):
     if pin == '-':
       return 0
     return int(pin)
 
-  def roll(self, pins):
-    self._frames = [self.pin_to_score(p) for p in pins]
+  def line(self, pins):
+    pins = [self.pin_to_int(p) for p in pins]
+    self._frames = list(zip(pins[::2], pins[1::2]))
 
   def score(self):
-    return sum(self._frames)
+    score = 0
+    for (a, b) in self._frames:
+      score += a + b
+    return score
--- bowling/test_bowling.py
+++ bowling/test_bowling.py
@@ -8,15 +8,14 @@ def game():
 def test_new_bowling_game(game):
   assert isinstance(game, object)
 
-def test_roll_numbers(game):
-  game.roll('123')
-  assert game._frames == [1, 2, 3]
-
+def test_line_to_frames(game):
+  game.line('1234')
+  assert game._frames == [(1, 2), (3, 4)]
 
 def test_score_1234(game):
-  game.roll('1234')
+  game.line('1234')
   assert game.score() == 10
 
 def test_miss(game):
-  game.roll('8-')
+  game.line('8-')
   assert game.score() == 8
