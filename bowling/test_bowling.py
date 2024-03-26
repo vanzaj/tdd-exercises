84521de9eca09c2d28afdb9d3981917dbb89d58d
Ivan <vanzaj@gmail.com>
2024-03-26 14:51:23 +0800

    too much

--- bowling/test_bowling.py
+++ bowling/test_bowling.py
@@ -11,10 +11,10 @@ def test_new_bowling_game(game):
     assert isinstance(game, object)
 
 
-def test_simple_rolls(game):
+def test_roll_one_frame(game):
     game.rolls([1, 2])
-    assert game._rolls == [1, 2]
     assert game.score() == 3
+
 
 def test_rolls_with_spare(game):
     game.rolls([9, 1, 2])
