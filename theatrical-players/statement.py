3c778912ac36f79812d81e3e56c3f3496b9f23ba
Ivan <vanzaj@gmail.com>
2024-04-24 17:22:13 +0800

    move extracted back inside statement

--- theatrical-players/statement.py
+++ theatrical-players/statement.py
@@ -1,40 +1,42 @@
 import math
 
-def is_comedy(play):
-    return play["type"] == "comedy"
-
-
-def amount_for(perf, play):
-    result = 0
-    if play["type"] == "tragedy":
-        result = 40000
-        if perf["audience"] > 30:
-            result += 1000 * (perf["audience"] - 30)
-    elif is_comedy(play):
-        result = 30000
-        if perf["audience"] > 20:
-            result += 10000 + 500 * (perf["audience"] - 20)
-        result += 300 * perf["audience"]
-    else:
-        raise ValueError(f'unknown type: {play["type"]}')
-    return result
-
-
-def calculate_volume_credits(nb_pax, is_comedy=False):
-    if is_comedy :
-        volume_credits = math.floor(nb_pax / 5)
-    else:
-        volume_credits = 0
-    return volume_credits + max(nb_pax - 30, 0)
 
 
 def statement(invoice, plays):
+    def format_as_dollars(cents):
+        return f"${cents/100:0,.2f}"
+
+
+    def is_comedy(play):
+        return play["type"] == "comedy"
+
+
+    def amount_for(aPerformance, play):
+        result = 0
+        if play["type"] == "tragedy":
+            result = 40000
+            if aPerformance["audience"] > 30:
+                result += 1000 * (aPerformance["audience"] - 30)
+        elif is_comedy(play):
+            result = 30000
+            if aPerformance["audience"] > 20:
+                result += 10000 + 500 * (aPerformance["audience"] - 20)
+            result += 300 * aPerformance["audience"]
+        else:
+            raise ValueError(f'unknown type: {play["type"]}')
+        return result
+
+
+    def calculate_volume_credits(nb_pax, is_comedy=False):
+        if is_comedy :
+            volume_credits = math.floor(nb_pax / 5)
+        else:
+            volume_credits = 0
+        return volume_credits + max(nb_pax - 30, 0)
+
     total_amount = 0
     volume_credits = 0
     result = f'Statement for {invoice["customer"]}\n'
-
-    def format_as_dollars(cents):
-        return f"${cents/100:0,.2f}"
 
     for perf in invoice["performances"]:
         play = plays[perf["playID"]]
