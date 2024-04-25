86e4d7e8a58b96775059aad5fbcf0a7ae6781977
Ivan <vanzaj@gmail.com>
2024-04-25 14:34:31 +0800

    revert to original statement.py

--- theatrical-players/statement.py
+++ theatrical-players/statement.py
@@ -1,61 +1,39 @@
 import math
 
 
-
 def statement(invoice, plays):
-    def usd(cents):
-        return f"${cents/100:0,.2f}"
+    total_amount = 0
+    volume_credits = 0
+    result = f'Statement for {invoice["customer"]}\n'
 
+    def format_as_dollars(amount):
+        return f"${amount:0,.2f}"
 
-    def is_comedy(play):
-        return play["type"] == "comedy"
-
-
-    def play_for(aPerformance):
-        return plays[aPerformance["playID"]]
-
-
-    def amount_for(aPerformance):
-        result = 0
-        play = play_for(aPerformance)
+    for perf in invoice["performances"]:
+        play = plays[perf["playID"]]
         if play["type"] == "tragedy":
-            result = 40000
-            if aPerformance["audience"] > 30:
-                result += 1000 * (aPerformance["audience"] - 30)
-        elif is_comedy(play):
-            result = 30000
-            if aPerformance["audience"] > 20:
-                result += 10000 + 500 * (aPerformance["audience"] - 20)
-            result += 300 * aPerformance["audience"]
+            this_amount = 40000
+            if perf["audience"] > 30:
+                this_amount += 1000 * (perf["audience"] - 30)
+        elif play["type"] == "comedy":
+            this_amount = 30000
+            if perf["audience"] > 20:
+                this_amount += 10000 + 500 * (perf["audience"] - 20)
+
+            this_amount += 300 * perf["audience"]
+
         else:
             raise ValueError(f'unknown type: {play["type"]}')
-        return result
 
+        # add volume credits
+        volume_credits += max(perf["audience"] - 30, 0)
+        # add extra credit for every ten comedy attendees
+        if "comedy" == play["type"]:
+            volume_credits += math.floor(perf["audience"] / 5)
+        # print line for this order
+        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+        total_amount += this_amount
 
-    def volume_credits_for(nb_pax, is_comedy=False):
-        result = math.floor(nb_pax / 5) if is_comedy else 0
-        return result + max(nb_pax - 30, 0)
-
-
-    def total_volume_credits():
-        result = 0
-        for perf in invoice["performances"]:
-            result += volume_credits_for(perf["audience"], is_comedy(play_for(perf)))
-        return result
-
-    def total_amount():
-        result = 0
-        for perf in invoice["performances"]:
-            result += amount_for(perf)
-        return result
-
-
-    result = f'Statement for {invoice["customer"]}\n'
-    for perf in invoice["performances"]:
-        result += f' {play_for(perf)["name"]}: {usd(amount_for(perf))} ({perf["audience"]} seats)\n'
-
-    result += f"Amount owed is {usd(total_amount())}\n"
-    result += f"You earned {total_volume_credits()} credits\n"
+    result += f"Amount owed is {format_as_dollars(total_amount/100)}\n"
+    result += f"You earned {volume_credits} credits\n"
     return result
-
-
