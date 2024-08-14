4de31f78405845858513aa30d3e0daf0bfe59288
Ivan <vanzaj@gmail.com>
2024-08-14 21:49:49 +0800

    rename

--- pytest_howto/tests/test_a_module.py
+++
@@ -1,5 +0,0 @@
-from a_package.a_module import a_function
-
-
-def test_a_function():
-    assert a_function() == 0
---
+++ pytest_howto/tests/test_module1.py
@@ -0,0 +1,5 @@
+from package_a.module1 import func_zero
+
+
+def test_a_function():
+    assert func_zero() == 0
