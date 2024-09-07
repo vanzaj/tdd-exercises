5c5d5f832832f273d92c45889dfa8b7975a7ca00
Ivan <vanzaj@gmail.com>
2024-09-07 17:56:58 +0800

    rename theatrical players

--- theatrical-players_old/statement.py
+++ theatrical-players_old/statement.py
@@ -9,32 +9,31 @@ def statement(invoice, plays):
     def format_as_dollars(amount):
         return f"${amount:0,.2f}"
 
-    for perf in invoice['performances']:
-        play = plays[perf['playID']]
-        if play['type'] == "tragedy":
+    for perf in invoice["performances"]:
+        play = plays[perf["playID"]]
+        if play["type"] == "tragedy":
             this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
+            if perf["audience"] > 30:
+                this_amount += 1000 * (perf["audience"] - 30)
+        elif play["type"] == "comedy":
             this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
+            if perf["audience"] > 20:
+                this_amount += 10000 + 500 * (perf["audience"] - 20)
 
-            this_amount += 300 * perf['audience']
+            this_amount += 300 * perf["audience"]
 
         else:
             raise ValueError(f'unknown type: {play["type"]}')
 
         # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
+        volume_credits += max(perf["audience"] - 30, 0)
         # add extra credit for every ten comedy attendees
         if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
+            volume_credits += math.floor(perf["audience"] / 5)
         # print line for this order
         result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
         total_amount += this_amount
 
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
-    result += f'You earned {volume_credits} credits\n'
+    result += f"Amount owed is {format_as_dollars(total_amount/100)}\n"
+    result += f"You earned {volume_credits} credits\n"
     return result
-
