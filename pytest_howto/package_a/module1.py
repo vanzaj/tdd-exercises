d59b10d415c622f6b0cdcf529fa72eed5d7e736d
Ivan <vanzaj@gmail.com>
2024-09-08 17:11:04 +0800

    add doc

--- pytest_howto/package_a/module1.py
+++ pytest_howto/package_a/module1.py
@@ -1,2 +1,7 @@
+# Example of a function inside module1 which is part of package_a
+# Q: How do we import and test it if tests are outside of the package?
+# A: Make sure $PYTHONPATH contains "." (which is not the case by default when running `pytest`)
+
 def func_zero():
     return 0
+
