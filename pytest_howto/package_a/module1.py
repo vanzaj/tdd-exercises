800842e1338c132ead4f1f66c978a14195da7556
Ivan <vanzaj@gmail.com>
2024-09-10 14:18:51 +0800

    test for exception

--- pytest_howto/package_a/module1.py
+++ pytest_howto/package_a/module1.py
@@ -5,3 +5,9 @@
 
 def func_zero():
     return 0
+
+
+def is_valid_email(email: str) -> bool | ValueError:
+    if "@" not in email:
+        raise ValueError(f"{email} is not valid")
+    return True
--- pytest_howto/tests/test_module1.py
+++ pytest_howto/tests/test_module1.py
@@ -1,5 +1,12 @@
-from package_a.module1 import func_zero
+import pytest
+from package_a.module1 import func_zero, is_valid_email
 
 
 def test_func_zero(fake_setup_and_teardown):
     assert func_zero() == 0
+
+
+def test_raises_exception():
+    with pytest.raises(ValueError) as ex:
+        is_valid_email("name-at-example.com")
+    assert "not valid" in str(ex.value)
