deca0823a4a60df6aa4ae64d095129382b86901f
Ivan <vanzaj@gmail.com>
2024-09-11 22:51:02 +0800

    test log capture

--- pytest_howto/package_a/module1.py
+++ pytest_howto/package_a/module1.py
@@ -1,6 +1,7 @@
 # Example of a function inside module1 which is part of package_a
 # Q: How do we import and test it if tests are outside of the package?
 # A: Make sure $PYTHONPATH contains "." (which is not the case by default when running `pytest`)
+import logging
 
 
 def func_zero():
@@ -8,6 +9,7 @@ def func_zero():
 
 
 def is_valid_email(email: str) -> bool | ValueError:
+    logging.info(f"validating {email}")
     if "@" not in email:
         raise ValueError(f"{email} is not valid")
     return True
--- pytest_howto/tests/test_module1.py
+++ pytest_howto/tests/test_module1.py
@@ -1,3 +1,4 @@
+import logging
 import pytest
 from package_a.module1 import func_zero, is_valid_email
 
@@ -10,6 +11,12 @@ def test_raises_exception():
     with pytest.raises(ValueError) as ex:
         is_valid_email("name-at-example.com")
     assert "not valid" in str(ex.value)
+
+
+def test_capture_log(caplog):
+    caplog.set_level(logging.INFO)
+    is_valid_email("name@example.com")
+    assert "validating" in caplog.text
 
 
 def test_parametrized_fixture(user_factory):
